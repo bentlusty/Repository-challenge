5bbfe92c5bfdb3372749a79bcca00e2a8ca0db98
Tlutsy <ben.tlusty@forter.com>
2021-08-30 12:01:11 +0300

    Removed app_service.py

--- repository_challenge/app_service.py
+++
@@ -1,9 +0,0 @@
-from repository_challenge import calculate_claims
-
-
-def process_claims_by_csv():
-    return calculate_claims.calculate_claims_by_csv()
-
-
-def process_claims():
-    return calculate_claims.calculate_claims()
--- repository_challenge/main.py
+++ repository_challenge/main.py
@@ -1,12 +1,12 @@
-from repository_challenge import app_service
+from repository_challenge import calculate_claims
 
 
 def main():
     print("Running main service")
     print("Running Processing claims by DB and ES")
-    print(app_service.process_claims())
+    print(calculate_claims.calculate_claims_by_csv())
     print("Running Processing claims by csv files")
-    print(app_service.process_claims_by_csv())
+    print(calculate_claims.calculate_claims())
 
 
 if __name__ == "__main__":
